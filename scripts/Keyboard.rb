b5f14800dd41a4f515fe2ecc023a2c3da94869bf
xtremeqg <xtremeq_g@hotmail.com>
2018-07-16 04:51:37 +0300

    Added keyboard module to allow for more inputs

---
+++ scripts/Keyboard.rb
@@ -0,0 +1,46 @@
+module Keyboard
+
+  @@getkeyboardstate = Win32API.new('user32', 'GetKeyboardState', ['P'], 'V')
+  @@state = nil
+  @@prev_state = nil
+
+  VK_SPACE = 0x20
+  VK_0 = 0x30
+  VK_1 = 0x31
+  VK_2 = 0x32
+  VK_3 = 0x33
+  VK_4 = 0x34
+  VK_5 = 0x35
+  VK_6 = 0x36
+  VK_7 = 0x37
+  VK_8 = 0x38
+  VK_9 = 0x39
+
+  def self.trigger?(which)
+    unless @@state.nil?
+      unless @@prev_state.nil?
+        return (@@state[which] & 0x80) > 0 && (@@prev_state[which] & 0x80) == 0
+      end
+      return @@state[which] > 0
+    end
+    return false
+  end
+
+  def self.update
+    @@prev_state = @@state
+    lpkeystate = ' ' * 256
+    @@getkeyboardstate.call(lpkeystate)
+    @@state = lpkeystate.unpack('C256')
+  end
+
+end
+
+module Input
+  class << self
+    alias_method :keyboard_update, :update
+    def update
+      keyboard_update
+      Keyboard.update
+    end
+  end
+end
