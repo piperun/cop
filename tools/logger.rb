eb064aa99361b085071270477baa67eb9f52cd52
xtremeqg <xtremeq_g@hotmail.com>
2018-07-21 00:25:52 +0300

    Expanded logger functionality

--- tools/logger.rb
+++ tools/logger.rb
@@ -1,18 +1,11 @@
 class Log
   @@stream = nil
-  def self.write(message)
-    if @@stream.nil?
-      @@stream = File.open('CG.log', 'w')
-    end
-    @@stream << message << "\n"
-  end
-end
-
-class Scene_Map
-  alias_method :orig_update, :update
-  def update
-    Log.write('frame')
-    return orig_update
+  @@prev_type = nil
+  def self.write(type, message = nil)
+    return if (type == 'frame') && (@@prev_type == 'frame')
+    @@stream = File.open('CG.log', 'w') if @@stream.nil?
+    @@stream << "#{type}: #{message}\n"
+    @@prev_type = type
   end
 end
 
@@ -20,8 +13,30 @@ class Interpreter
   alias_method :orig_command_101, :command_101
   def command_101
     res = orig_command_101
-    Log.write("message " + $game_temp.message_text.gsub("\n", ' '))
+    Log.write('message', $game_temp.message_text.gsub("\n", ' '))
     return res
+  end
+end
+
+module Graphics
+  class << self
+    alias_method :log_update, :update
+    @@framecounter = 0
+    def update
+      Log.write('frame', @@framecounter)
+      @@framecounter += 1
+      return log_update
+    end
+  end
+end
+
+module Audio
+  class << self
+    alias_method :log_se_play, :se_play
+    def se_play(filename, volume = 100, pitch = 100)
+      Log.write('sfx', filename)
+      return log_se_play(filename, volume, pitch)
+    end
   end
 end
 
@@ -30,7 +45,7 @@ module RPG
     class << self
       alias_method :log_picture, :picture
       def picture(filename)
-        Log.write(filename)
+        Log.write('picture', filename) unless filename.empty?
         return log_picture(filename)
       end
     end
